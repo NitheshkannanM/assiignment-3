41d8fdc09d32a89cdd8f65c5d044dee3da118d66
Nitheshkannan. M <52233898+NitheshkannanM@users.noreply.github.com>
2022-01-21 18:07:18 +0530

    Add files via upload

---
+++ asssignment3.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 5; i++) {
+	console.log(i);
+};
+const a = [ 'j','a','v','a' ];
+
+for (let x in a) {
+    console.log(a[x]);
+}
+let b = ["car", "bike", "cycle", "bus"];
+
+for (let b of b) {
+    console.log(b);
+};
+const c = [ 'j','a','v','a'];
+c.forEach(element => 
+    console.log(element));
